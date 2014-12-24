dd30b04fd7c8df31aebfcc718e122d80e4c384bc
gorrotowi <sebastian.tellez.o@gmail.com>
2014-12-24 15:30:25 -0600

    Change to Jade and Less

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "css/main.css": "less/main.less"
+        }
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          pretty: false
+        },
+        files: {
+          // target.css file: source.less file
+          "index.html": "jade/index.jade"
+        }
+      }
+    },
+    watch: {
+      jade: {
+        files: ['jade/**/*.jade'], // which files to watch
+        tasks: ['jade'],
+        options: {
+          nospawn: true
+        }
+      },
+      less: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
