f6477e8810ee5fda35b5bbbee1536777965b72da
gorrotowi <sebastian.tellez.o@gmail.com>
2014-12-25 22:28:54 -0600

    All most finished ;)

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
         files: {
           // target.css file: source.less file
           "css/main.css": "less/main.less",
-          "css/default.css":"less/default.less"
+          "css/default.css":"less/default.less",
+          "css/media-queries.css":"less/media-queries.less"
         }
       }
     },
